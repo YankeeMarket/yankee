536160413e03eff5d5f4cc35c179ca05e2bf79d2
Dave Block <dave@northcreek.ca>
2018-03-02 18:51:22 -0700

    wrong syntax for default db connection

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('pgsql', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
